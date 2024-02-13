6103c62c9d599f54828827b79dad3bf8b296279c
francesco-pertica <157395900+francesco-pertica@users.noreply.github.com>
2024-02-13 10:18:26 +0100

    Update Port_scanner.py

--- Port_scanner.py
+++ Port_scanner.py
@@ -1,27 +1,28 @@
 import socket
 
-def scan_ports(target, start_port, end_port):
-    open_ports = []
+open_ports = []
+
+def scan_ports(target, start_port, end_port)
 
     for port in range(start_port, end_port + 1):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        result = sock.connect_ex((target, port)) #
+        result = sock.connect_ex((target, port)) 
 
         if result == 0:
-            open_ports.append(port)
+            print(f"Porta {port} - Aperta")
+	else : 
+	    print(f"Porta {port} - Chiusa")
+		
+	sock.close()
 
-        sock.close()
+    return 0
 
-    return open_ports
-
-target_host = "192.168.50.101" #input("Inserisci l'indirizzo IP del target: ")
+target_host = input("Inserisci l'indirizzo IP del target: ")
 start_port = int(input("Inserisci la porta iniziale dello scan: "))
 end_port = int(input("Inserisci la porta finale dello scan: "))
 
-open_ports = scan_ports(target_host, start_port, end_port)
+scan_ports(target_host, start_port, end_port)
 
-if open_ports:
-	print(f"Porte aperte su {target_host}: {open_ports}")
-else:
+if not open_ports:
         print(f"Nessuna porta aperta trovata su {target_host}")
