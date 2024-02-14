5ed4817968ecb97e247d0be5ede1d4f1ea641c20
ManuelDiGangi <157116580+ManuelDiGangi@users.noreply.github.com>
2024-02-14 10:14:57 +0100

    Update Port_scanner.py

--- Port_scanner.py
+++ Port_scanner.py
@@ -5,7 +5,9 @@ open_ports = []
 def scan_ports(target, start_port, end_port):
 
     for port in range(start_port, end_port + 1):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+	    #utilizzo protocollo tcp perchè l'udp è un protocollo connectionless e di conseguenza 
+	    #non è possibile verificare la risposta della porta
 
         result = sock.connect_ex((target, port)) 
 
